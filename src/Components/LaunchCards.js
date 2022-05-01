240b75f0f4f55e1b6947e0d79be3819a985d7361
avniraghuvanshi <raghuvanshiavni145@gmail.com>
2022-05-01 06:39:12 -0700

    added no results messsage

--- src/Components/LaunchCards.js
+++ src/Components/LaunchCards.js
@@ -2,24 +2,23 @@ import React from "react";
 import './LaunchCards.css';
 
 const LaunchCards = ({result}) =>{
-    console.log(result);
     const launch_cards = result[0];
     const cards = launch_cards.map(card=>{
-        const missionId = card.mission_id ? [...card.mission_id] : '-';
+        const missionId = card.mission_id ? [...card.mission_id] : "-" ;
         const launchYear = card.launch_year ? card.launch_year : '-';
         const launchSuccess = card.launch_success ? 'True' : 'False';
         const landSuccess = card.rocket.first_stage.cores[0].land_success ? 'True' : 'False';
         return(
             <div key={card?.flight_number} className="card">
                 <div className="img-container">
-                    <img src={card?.links.mission_patch}></img>
+                    <img alt={card?.mission_name} src={card?.links.mission_patch}></img>
                 </div>
                 <div className="card-content">
                     <h1 className="content-title">{card?.mission_name}</h1>
                     <div className="details">
                         <span>Mission Id</span>
                         <ul>
-                            {''}
+                            {" "}
                             {missionId.map((id , index)=>{
                                 return <li key={index}>{id}</li>
                             })}
@@ -44,7 +43,7 @@ const LaunchCards = ({result}) =>{
 
     return(
         <section className="grid-container">
-            {cards}
+            {launch_cards.length > 0 ? cards : <h1 style={{fontSize: "2.5rem" , placeSelf:"center" , fontWeight:"300"}}>No Results Found</h1>}
         </section>
     );
 }
